f143daf44cf86ce769cd14118f60f7674e87b3d8
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2019-12-29 02:01:23 +0100

    Added views for basic viewing and hooked up urls

--- vinyls/urls.py
+++ vinyls/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from vinyls import views
 
 urlpatterns = [
-    path(),
+    path('genres/', views.GenreListView.as_view(), name='genres_list'),
+    path('albums/', views.AlbumListView.as_view(), name='albums_list'),
+    path('albums/<int:pk>/', views.AlbumDetailView.as_view(), name='album_detail'),
+    path('tracks/', views.TrackListView.as_view(), name='tracks_list'),
+    path('reviews/', views.ReviewListView.as_view(), name='reviews_list'),
+    path('reviews/<int:pk>/', views.ReviewDetailView.as_view(), name='review_detail'),
+    path('shopping-cart/<int:pk>', views.ShoppingCartView.as_view(), name='shopping_cart'),
+    path('shopping-cart-items/', views.ShoppingCartItemListView.as_view(), name='shopping_cart_items_list'),
 ]
--- vinyls/views.py
+++ vinyls/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics
+from vinyls import models, serializers
 
-# Create your views here.
+
+class GenreListView(generics.ListAPIView):
+    queryset = models.Genre.objects.all()
+    serializer_class = serializers.GenreSerializer
+
+
+class AlbumListView(generics.ListAPIView):
+    queryset = models.Album.objects.all()
+    serializer_class = serializers.AlbumSerializer
+
+
+class AlbumDetailView(generics.RetrieveAPIView):
+    queryset = models.Album.objects.all()
+    serializer_class = serializers.AlbumSerializer
+
+
+class TrackListView(generics.ListAPIView):
+    queryset = models.Track.objects.all()
+    serializer_class = serializers.TrackSerializer
+
+
+class ReviewListView(generics.ListAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+
+class ReviewDetailView(generics.RetrieveAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+
+class ShoppingCartView(generics.RetrieveAPIView):
+    queryset = models.ShoppingCart.objects.all()
+    serializer_class = serializers.ShoppingCartSerializer
+
+
+class ShoppingCartItemListView(generics.ListAPIView):
+    queryset = models.ShoppingCartItem.objects.all()
+    serializer_class = serializers.ShoppingCartSerializer
