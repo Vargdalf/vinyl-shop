c8c22fe898a1b611e34bfca921ae1751cc4a71ba
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2019-12-30 23:53:31 +0100

    Expanded models a bit with methods and helpers

--- vinyls/models.py
+++ vinyls/models.py
@@ -46,16 +46,39 @@ class Review(models.Model):
         return f'{self.rating}/5 - {self.owner}'
 
 
-# TODO: clear_basket, add_product, get_total, is_empty, total_discount,
+# TODO: get_total, total_discount
 class ShoppingCart(models.Model):
     owner = models.ForeignKey(User, related_name='cart', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.owner}\'s Cart'
 
+    def clear_cart(self):
+        self.items.all().delete()
+
+    def add_album(self, album):
+        if not self.id:
+            self.save()
+        items = self.items.filter(album=album)
+        if len(items) == 0:
+            self.items.create(album=album)
+        else:
+            item = items[0]
+            item.quantity += 1
+            item.save()
+
+    def is_empty(self):
+        return self.id is None or self.num_items == 0
+
+    def num_items(self):
+        return self.all_items().count()
+
+    def all_items(self):
+        return self.items.all()
+
 
 class ShoppingCartItem(models.Model):
-    cart = models.ForeignKey(ShoppingCart, related_name='cart_items', on_delete=models.CASCADE)
+    cart = models.ForeignKey(ShoppingCart, related_name='items', on_delete=models.CASCADE)
     album = models.ForeignKey(Album, related_name='shopping_cart_items', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -72,16 +95,24 @@ class Profile(models.Model):
     wish_list = ''
 
 
-# TODO: add_product
 class WishList(models.Model):
     owner = models.ForeignKey(User, related_name='wishlist', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.owner}\'s Wishlist'
 
+    def add_album(self, album):
+        items = self.items.filter(album=album)
+        if len(items) == 0:
+            self.items.create(album=album)
+        else:
+            item = items[0]
+            item.quantity += 1
+            item.save()
+
 
 class WishListItem(models.Model):
-    wishlist = models.ForeignKey(WishList, related_name='list_items', on_delete=models.CASCADE)
+    wishlist = models.ForeignKey(WishList, related_name='items', on_delete=models.CASCADE)
     album = models.ForeignKey(Album, related_name='wishlist_items', on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
 
