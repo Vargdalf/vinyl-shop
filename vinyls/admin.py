f1f9c48abc325e98b7bbc29ef3d6e62266f5a5d1
Kacper Mieszala <24653160+Vargdalf@users.noreply.github.com>
2020-01-06 21:01:43 +0100

    Added custom user to admin panel

--- vinyls/admin.py
+++ vinyls/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from vinyls.models import Genre, Album, Track, Review, User
+from django.contrib.auth.admin import UserAdmin
+from vinyls.models import User, Genre, Album, Track, Review
 
+admin.site.register(User, UserAdmin)
 admin.site.register(Genre)
 admin.site.register(Album)
 admin.site.register(Track)
 admin.site.register(Review)
-admin.site.register(User)
